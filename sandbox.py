a0db2d2a79b1ddc1492eb1aa95068af8d34cb445
janphiliprichter <janphiliprichter@gmail.com>
2023-10-06 22:08:01 +0200

    First data and model pipline

--- sandbox.py
+++ sandbox.py
@@ -1,18 +1,120 @@
 import os
-from helper_functions import corrupted_images
-from helper_functions import get_image_paths
-from helper_functions import plot_images
+import numpy as np
+import tensorflow as tf
+from helper_functions import corrupted_images, get_image_paths, plot_images, history_to_json, json_loader, plot_metrics
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Activation
+from tensorflow.keras import Sequential
 
+print("")
 # Set working directory
+print("Setting working directory")
 cwd = os.getcwd()
+print("")
 
 # Getting list of all images
+print("Obtaining image file paths")
 image_paths = get_image_paths(directory=cwd)
+print("")
+
 
 # Checking for corrupted and non-jpg images
+print("Checking for corrupted and non-jpg images in the image files")
 non_jpgs, corrupted = corrupted_images(images=image_paths)
+print("")
 
 # Showing random images
+print("Plotting a grid of random images")
 plot_images(image_paths)
+print("")
+
+# Loading data set
+print("Loading in the dataset as tensor batches")
+print("")
+dir_path = "/Users/philip/Documents/Milano University/2. Semester/Machine Learning/ML Project/data/train"
+
+train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(dir_path,
+                                                               label_mode="binary",
+                                                               color_mode="rgb",
+                                                               batch_size=32,
+                                                               image_size=(128, 128),
+                                                               shuffle=True,
+                                                               seed=42,
+                                                               subset="both",
+                                                               validation_split=0.25)
 
 
+# Printing shapes of the elements in the training data set
+print("")
+print("Information about the tensor batches:")
+print("")
+for image_batch, labels_batch in train_ds:
+    print(f"The training data set contains {len(train_ds)} batches")
+    print("")
+    print(f"Shape of an image batch: {image_batch.shape}")
+    print(f"Shape of a label batch: {labels_batch.shape}")
+    print("")
+    print("Tensor of an image in the batch:")
+    print(image_batch[0])
+    print("")
+    print(f"Maximum and minimum values of the image: {np.min(image_batch[0]), np.max(image_batch[0])}")
+    print("")
+    break
+
+print("Normalising the data")
+normalisation_layer = tf.keras.layers.Rescaling(1. / 255)
+train_ds = train_ds.map(lambda x, y: (normalisation_layer(x), y))
+val_ds = val_ds.map(lambda x, y: (normalisation_layer(x), y))
+image_batch, labels_batch = next(iter(train_ds))
+first_image = image_batch[0]
+print(f"New maximum and minimum values of an image: {np.min(first_image), np.max(first_image)}")
+print("")
+
+
+print("Defining the model")
+print("")
+model = Sequential([
+    Conv2D(10, 3, activation="relu", input_shape=(128, 128, 3)),
+    MaxPool2D(pool_size=(2, 2)),
+    Conv2D(10, 3, activation="relu"),
+    MaxPool2D(pool_size=(2, 2)),
+    Conv2D(10, 3, activation="relu"),
+    MaxPool2D(pool_size=(2, 2)),
+    Flatten(),
+    Dense(1, activation="sigmoid")
+])
+
+print("Compiling the model")
+print("")
+model.compile(loss="binary_crossentropy",
+              optimizer=Adam(),
+              metrics=["accuracy"])
+
+# Fitting the model
+print("Fitting the model")
+history = model.fit(train_ds,
+                    epochs=10,
+                    steps_per_epoch=len(train_ds),
+                    validation_data=val_ds,
+                    validation_steps=len(val_ds))
+
+
+# Saving the history to a json file
+history_file_path = "/Users/philip/Documents/Milano University/2. Semester/Machine Learning/ML " \
+                    "Project/hists/history.json"
+
+print("Saving model history to json file")
+print("")
+history_to_json(history=history, file_path=history_file_path)
+
+# Loading json file of the models history
+print("Loading model history to json file")
+print("")
+history_dict = json_loader(history_file_path)
+
+print(f"Dictionary of the models history loaded from the json file: {history_dict}")
+print("")
+
+# Plotting metrics curves
+print("Plotting loss and accuracy curves of the model")
+plot_metrics(history=history_dict)
